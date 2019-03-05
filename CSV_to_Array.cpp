de53a817643e5324c243439a2fdc0b5bdc47c42b
Victor Wang <45219858+vdoubleu@users.noreply.github.com>
2019-03-04 20:16:29 -0500

    Update CSV_to_Array.cpp

--- CSV_to_Array.cpp
+++ CSV_to_Array.cpp
@@ -9,74 +9,13 @@
 
 
 int main()
-{/*
-	//const std::string file_path = "/usd/test.csv";
-	const std::string file_path = "C:/Users/victor/Desktop/test.csv";
-
-	std::string temp = "";
-
-	int array_size = 5; //number of elements per element
-
-	std::ifstream CSV_file;
-	CSV_file.open(file_path);
-
-	int line_count = 0;
-
-	//counts number of lines in file
-	while (getline(CSV_file, temp)) {
-		line_count++;
-	}
-
-	line_count = (line_count + 1) / 2;
-
-	//creates array of correct size
-	double** numArr = new double*[line_count];
-
-	for (int i = 0; i < line_count; i++){
-		numArr[i] = new double[array_size];
-	}
-
-	//resets file
-	CSV_file.clear();
-	CSV_file.seekg(0);
-
-	//start to take things from CSV
-	int i = 0;
-	while (getline(CSV_file, temp)) {
-		if (i % 2 == 0) {
-			for (int j = 0; j < array_size; j++) {
-				std::size_t location = 0;
-				location = temp.find(",");
-
-				if (location != std::string::npos) {
-					numArr[i/2][j] = stod(temp.substr(0, location));
-					temp = temp.substr(location + 1, temp.length() - 1 - location);
-				}
-				else {
-					numArr[i/2][j] = stod(temp);
-				}
-				
-			}
-		}
-		i++;
-	}
-*/
-
-
-	
-	//char file_path[] = "C:/Users/victor/Desktop/test.csv";
-
+{	
 	const std::string file_path = "C:/Users/victor/Desktop/test.csv";
 	
 	
 	std::string line = "";
 	std::ifstream csv_file;
 	csv_file.open(file_path);
-	
-	//char string[100];
-	//FILE* usd_file_read = fopen(file_path, "r");
-
-	//gets(string);
 
 	int array_width = 8; //number of values that need to be parsed per line
 	int* array_width_ptr = &array_width;
@@ -89,11 +28,6 @@ int main()
 	while (getline(csv_file, line)) {
 		line_count++;
 	}
-	
-	/*while (string[c] != '\0') {
-
-	}*/
-
 
 	//creates array of correct size
 	double** profile_info = new double*[line_count];
@@ -131,8 +65,6 @@ int main()
 		}
 		std::cout << std::endl;
 	}
-
-	//std::cout << line_count << " :linecount:" << std::endl;
 
 
 }
